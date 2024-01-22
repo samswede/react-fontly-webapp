2a4d958a008e2c0839b17378ee7ce0f1420cdead
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2024-01-22 17:52:27 +0100

    adding request hooks

---
+++ my-nextui-project/client/src/hooks/requests.js
@@ -0,0 +1,40 @@
+/*
+// before we were using docker...
+const API_URL = 'http://localhost:8000/v1'; // v1 API route.
+*/
+// less specific when using docker...
+const API_URL = 'v1';
+
+// Load fonts and return as JSON.
+async function httpGetFonts() {
+  const response = await fetch(`${API_URL}/fonts`)
+  
+  return await response.json();
+}
+
+// Submit drawing to server
+async function httpSketchSearch(sketch) {
+  // fetch uses the GET method by default, so we need to specify the POST method.
+  // We also need to specify the Content-Type header, so that the server knows what kind of data we are sending.
+  // We also need to stringify the data, because fetch only accepts strings.
+  try {
+    return await fetch(`${API_URL}/encoder`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(sketch),
+    });
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
+  
+}
+
+
+export {
+  httpGetFonts,
+  httpSketchSearch,
+};
